7f479d67279e488705dbf3584d8e1bee44202ae4
MarwaneQ <merouane12345bensadallah@gmail.com>
2024-07-31 19:19:32 +0100

    sending a response

--- index.js
+++ index.js
@@ -12,9 +12,15 @@
 //   console.log(data);
 // })
 // console.log("File written!");
+// creating a server and sending a response.
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req.headers);
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My Webpage</title></head>");
+  res.write("<body><h1>Hello, World!</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 server.listen(3000);
