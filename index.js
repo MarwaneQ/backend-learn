cc8f5c36b08f26413b6e55519f436d2a351108da
MarwaneQ <merouane12345bensadallah@gmail.com>
2024-08-01 15:02:42 +0100

    routing requests

--- index.js
+++ index.js
@@ -16,6 +16,14 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
+  const url = req.url
+  if (url ==='/'){
+    res.write("<html>");
+  res.write("<head><title>enter message</title></head>");
+  res.write("<body><form action = '/message' method='POST'><input name='message'><button type='submit'>send</button></form></body>");
+  res.write("</html>");
+  res.end();
+  }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>My Webpage</title></head>");
