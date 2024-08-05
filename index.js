5a5be9e95ffdbfdd2cecb0f23eb8cc869cd696eb
MarwaneQ <merouane12345bensadallah@gmail.com>
2024-08-05 21:28:35 +0100

    setting-up express

--- index.js
+++ index.js
@@ -19,6 +19,8 @@
 // const server = http.createServer(routes.handler);
 // server.listen(3000);
 const http = require("http");
+const express = require("express");
 const routes = require("./routes");
-const server = http.createServer(routes.handler);
+const app = express();
+const server = http.createServer(app);
 server.listen(3000);
