7696c5b7e4f62f59c3b43541f67d61d27b9a8fc1
MarwaneQ <merouane12345bensadallah@gmail.com>
2024-08-05 10:45:06 +0100

    node modules system

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // const { log } = require("console");
-// const fs = require("fs");
+
 // //blocking, synchronous way
 // // fs module is a built-in module in Node.js that allows you to work with the file system on your computer.
 // const textIn = fs.readFileSync("./text/input.txt", "utf-8");
@@ -15,20 +15,6 @@
 // creating a server and sending a response.
 const http = require("http");
 
-const server = http.createServer((req, res) => {
-  const url = req.url
-  if (url ==='/'){
-    res.write("<html>");
-  res.write("<head><title>enter message</title></head>");
-  res.write("<body><form action = '/message' method='POST'><input name='message'><button type='submit'>send</button></form></body>");
-  res.write("</html>");
-  res.end();
-  }
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>My Webpage</title></head>");
-  res.write("<body><h1>Hello, World!</h1></body>");
-  res.write("</html>");
-  res.end();
-});
+const routes = require("./routes");
+const server = http.createServer(routes);
 server.listen(3000);
