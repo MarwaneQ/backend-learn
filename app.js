1c128fa1b70a404f41363545c0fd6ccaee327e58
MarwaneQ <merouane12345bensadallah@gmail.com>
2024-08-08 18:51:56 +0100

    adding 404 page

--- app.js
+++ app.js
@@ -36,4 +36,7 @@ const app = express();
 app.use(bodyparser.urlencoded({extended:false}));
 app.use(adminRoutes);
 app.use(shopRoutes)
+app.use((req,res,next)=>{
+  res.status(404).send('<h1>Page not found</h1>');
+})
 app.listen(3000);
