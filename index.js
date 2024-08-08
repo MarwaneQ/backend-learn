1c446840a3daa1605d9e9bbe946cf71b0e77ea41
MarwaneQ <merouane12345bensadallah@gmail.com>
2024-08-08 12:34:27 +0100

    middleware

--- index.js
+++ index.js
@@ -22,5 +22,13 @@ const http = require("http");
 const express = require("express");
 const routes = require("./routes");
 const app = express();
+app.use((req,res,next)=>{
+  console.log("In the middleware!");
+  next(); // alows the request to continue to the next middleware in line
+});
+app.use((req,res,next)=>{
+console.log("In another middleware!");
+res.send("<h1>Hello from Express!</h1>");
+})
 const server = http.createServer(app);
 server.listen(3000);
