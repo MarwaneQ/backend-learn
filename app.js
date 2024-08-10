9dbc40211d01be50492042dca8f6129240a32dee
MarwaneQ <merouane12345bensadallah@gmail.com>
2024-08-10 13:18:50 +0100

    filtering path

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ const shopRoutes = require('./routes/shop');
 const bodyparser = require('body-parser');
 const app = express();
 app.use(bodyparser.urlencoded({extended:false}));
-app.use(adminRoutes);
+app.use('/admin',adminRoutes); //filtering path
 app.use(shopRoutes)
 app.use((req,res,next)=>{
   res.status(404).send('<h1>Page not found</h1>');
