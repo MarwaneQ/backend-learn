a21b9f290a93d700e25543ade10e57775638ae64
MarwaneQ <merouane12345bensadallah@gmail.com>
2024-08-10 14:45:54 +0100

    helper function for navigation

--- app.js
+++ app.js
@@ -28,7 +28,7 @@
 // })
 // app.listen(3000);
 const express = require('express');
-
+const path = require('path');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const bodyparser = require('body-parser');
@@ -37,6 +37,6 @@ app.use(bodyparser.urlencoded({extended:false}));
 app.use('/admin',adminRoutes); //filtering path
 app.use(shopRoutes)
 app.use((req,res,next)=>{
-  res.status(404).send('<h1>Page not found</h1>');
+  res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 })
 app.listen(3000);
