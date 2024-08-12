e165a90c60bc95bf6cf19ea4c4b1c9513605affe
MarwaneQ <merouane12345bensadallah@gmail.com>
2024-08-12 16:55:32 +0100

    implementing pug

--- app.js
+++ app.js
@@ -27,17 +27,18 @@
 // res.send("<h1>Hello from Express!</h1>");
 // })
 // app.listen(3000);
-const express = require('express');
-const path = require('path');
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-const bodyparser = require('body-parser');
+const express = require("express"); //importing express
+const path = require("path");
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const bodyparser = require("body-parser");
 const app = express();
-app.use(bodyparser.urlencoded({extended:false})); //parsing the body
-app.use(express.static(path.join(__dirname,'public'))); //filtering path
-app.use('/admin',adminRoutes.routes); //filtering path
-app.use(shopRoutes)
-app.use((req,res,next)=>{
-  res.status(404).sendFile(path.join(__dirname,'views','404.html'));
-})
+app.set("view engine", "pug"); //setting view engine
+app.use(bodyparser.urlencoded({ extended: false })); //parsing the body
+app.use(express.static(path.join(__dirname, "public"))); //filtering path
+app.use("/admin", adminRoutes.routes); //filtering path
+app.use(shopRoutes);
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
 app.listen(3000);
