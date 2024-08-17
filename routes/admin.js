3fe88f0781844533eaf2229a65d5f294e9d7493e
MarwaneQ <merouane12345bensadallah@gmail.com>
2024-08-17 17:17:01 +0100

    adding post in controller and the product empty array

--- routes/admin.js
+++ routes/admin.js
@@ -2,26 +2,14 @@ const path = require('path'); // Import path
 
 const express = require('express'); // Import express
 
-const router = express.Router(); // Create a router object
+const productsController = require('../controllers/products'); // Import the products controller
 
-const products = []; // Create an empty array for products
+const router = express.Router(); // Create a router object
+ // Create an empty array for products
 
 // /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
-  res.render('add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
-  });
-});
+router.get('/add-product', productsController.getAddProduct); // Handle GET requests to /admin/add-product
 
 // /admin/add-product => POST
-router.post('/add-product', (req, res, next) => {
-  products.push({ title: req.body.title });
-  res.redirect('/');
-});
-
-exports.routes = router;
-exports.products = products;
+router.post('/add-product', productsController.postAddProduct);
+module.exports = router; // Export the router
