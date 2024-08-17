6e35cbdc30bcac52fabe66d466090344f5b4a13c
MarwaneQ <merouane12345bensadallah@gmail.com>
2024-08-17 18:42:53 +0100

    storing data in files via the model

--- models/product.js
+++ models/product.js
@@ -1,3 +1,5 @@
+const path = require("path");
+const fs = require("fs");
 const products = []; // Create an empty array for products
 
 module.exports = class Product {
@@ -6,10 +8,29 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this);
+    const product = path.join(
+      path.dirname(require.main.filename),
+      "data",
+      "products.json"
+    );
+    fs.readFile(product, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent); // Parse the file content to JSON format if there is no error
+      }
+      products.push(this);
+      fs.writeFile(product, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
   }
 
   static fetchAll() {
-    return products;
+    fs.readFile(product, (err, fileContent) => {
+      if (err) {
+        return [];
+      }
+      return JSON.parse(fileContent);
+    });
   }
-}
+};
