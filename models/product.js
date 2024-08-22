06c9f9ac6221d008819bc98e87ab2594a3b666da
MarwaneQ <merouane12345bensadallah@gmail.com>
2024-08-22 19:44:32 +0100

    defining a model

--- models/product.js
+++ models/product.js
@@ -1,36 +1,27 @@
-const db = require("../util/database");
-const path = require("path");
-const Cart = require("./cart");
+const Sequelize = require("sequelize");
 
-// const p = path.join(
-//   path.dirname(process.mainModule.filename),
-//   "data",
-//   "products.json"
-// );
+const sequelize = require("../util/database");
 
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
-  }
+const Product = sequelize.define("product", { // Product is the name of the table
+  id: { // id is the name of the column
+    type: Sequelize.INTEGER, // INTEGER is a data type in SQL
+    autoIncrement: true, // autoIncrement is a property that makes the id increase by 1 automatically
+    allowNull: false, // allowNull is a property that makes the id required
+    primaryKey: true, // primaryKey is a property that makes the id the primary key
+  },
+  title: Sequelize.STRING, // title is the name of the column
+  price: { // price is the name of the column
+    type: Sequelize.DOUBLE, // DOUBLE is a data type in SQL
+    allowNull: false, // allowNull is a property that makes the price required
+  },
+  imageUrl: { // imageUrl is the name of the column
+    type: Sequelize.STRING, // STRING is a data type in SQL
+    allowNull: false, // allowNull is a property that makes the imageUrl required
+  },
+  description: { // description is the name of the column
+    type: Sequelize.STRING, // STRING is a data type in SQL
+    allowNull: false, // allowNull is a property that makes the description required
+  },
+}); 
 
-  save() {
-    return db.execute(
-      "INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)",
-      [this.title, this.price, this.imageUrl, this.description]
-    ); // This is a promise because of the .execute() method in database.js file which returns a promise
-  }
-
-  static deleteById(id) {}
-
-  static fetchAll() {
-    return db.execute("SELECT * FROM products");
-  }
-
-  static findById(id) {
-    return db.execute("SELECT * FROM products WHERE products.id = ?", [id]); // This is a promise because of the .execute() method in database.js file which returns a promise
-  }
-};
+module.exports = Product; 
