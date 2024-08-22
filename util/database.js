ed16428cc22ff5ea11c80fc973183302ea4ac1f8
MarwaneQ <merouane12345bensadallah@gmail.com>
2024-08-22 19:35:02 +0100

    exporting

--- util/database.js
+++ util/database.js
@@ -1,6 +1,7 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize('node ', 'root', 'password', {
-  dialect: 'mysql',
-  host: 'localhost'
+const sequelize = new Sequelize("node ", "root", "password", {
+  dialect: "mysql",
+  host: "localhost",
 });
+module.exports = sequelize;
