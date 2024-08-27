9b87d0683c1e7eddde9a78173c0d01c11daf651e
MarwaneQ <merouane12345bensadallah@gmail.com>
2024-08-27 17:02:42 +0100

    edit and delet

--- models/product.js
+++ models/product.js
@@ -1,76 +1,64 @@
-// const Sequelize = require("sequelize");
+const mongodb = require('mongodb');
+const getDb = require('../util/database').getDb;
 
-// const sequelize = require("../util/database");
-
-// const Product = sequelize.define("product", { // Product is the name of the table
-//   id: { // id is the name of the column
-//     type: Sequelize.INTEGER, // INTEGER is a data type in SQL
-//     autoIncrement: true, // autoIncrement is a property that makes the id increase by 1 automatically
-//     allowNull: false, // allowNull is a property that makes the id required
-//     primaryKey: true, // primaryKey is a property that makes the id the primary key
-//   },
-//   title: Sequelize.STRING, // title is the name of the column
-//   price: { // price is the name of the column
-//     type: Sequelize.DOUBLE, // DOUBLE is a data type in SQL
-//     allowNull: false, // allowNull is a property that makes the price required
-//   },
-//   imageUrl: { // imageUrl is the name of the column
-//     type: Sequelize.STRING, // STRING is a data type in SQL
-//     allowNull: false, // allowNull is a property that makes the imageUrl required
-//   },
-//   description: { // description is the name of the column
-//     type: Sequelize.STRING, // STRING is a data type in SQL
-//     allowNull: false, // allowNull is a property that makes the description required
-//   },
-// });
-const {ObjectId} = require("mongodb");
-const getDb = require("../util/database").getDb;
 class Product {
-  constructor(title, imageUrl, description, price) {
+  constructor(title, price, description, imageUrl, id) {
     this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
     this.price = price;
+    this.description = description;
+    this.imageUrl = imageUrl;
+    this._id = id;
   }
+
   save() {
     const db = getDb();
-    return db
-      .collection("products")
-      .insertOne(this)
-      .then((result) => {
+    let dbOp;
+    if (this._id) {
+      // Update the product
+      dbOp = db
+        .collection('products')
+        .updateOne({ _id: new mongodb.ObjectId(this._id) }, { $set: this });
+    } else {
+      dbOp = db.collection('products').insertOne(this);
+    }
+    return dbOp
+      .then(result => {
         console.log(result);
       })
-      .catch((err) => {
+      .catch(err => {
         console.log(err);
       });
   }
+
   static fetchAll() {
     const db = getDb();
     return db
-      .collection("products")
+      .collection('products')
       .find()
       .toArray()
-      .then((products) => {
+      .then(products => {
         console.log(products);
         return products;
       })
-      .catch((err) => {
+      .catch(err => {
         console.log(err);
       });
   }
+
   static findById(prodId) {
     const db = getDb();
     return db
-      .collection("products")
-      .find({ _id: new ObjectId(prodId) })
+      .collection('products')
+      .find({ _id: new mongodb.ObjectId(prodId) })
       .next()
-      .then((product) => {
+      .then(product => {
         console.log(product);
         return product;
       })
-      .catch((err) => {
+      .catch(err => {
         console.log(err);
       });
   }
 }
+
 module.exports = Product;
--- util/database.js
+++ util/database.js
@@ -1,26 +1,29 @@
-const mongoClient = require("mongodb").MongoClient;
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
 let _db;
-const mongoConnect = (callback) => {
-  mongoClient
-    .connect(
-      "mongodb+srv://node-user:db-pass@cluster-0.1b4j4.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster-0"
-    )
-    .then((client) => {
+
+const mongoConnect = callback => {
+  MongoClient.connect(
+    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop?retryWrites=true'
+  )
+    .then(client => {
+      console.log('Connected!');
       _db = client.db();
-      console.log("Connected");
-      callback(client);
+      callback();
     })
-    .catch((err) => {
+    .catch(err => {
       console.log(err);
       throw err;
     });
 };
+
 const getDb = () => {
   if (_db) {
     return _db;
   }
-  throw "No database found!";
-}
+  throw 'No database found!';
+};
 
 exports.mongoConnect = mongoConnect;
 exports.getDb = getDb;
