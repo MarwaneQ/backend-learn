6ddef62455ae7d6ae044952d17909c883a10dfdf
MarwaneQ <merouane12345bensadallah@gmail.com>
2024-08-28 11:35:13 +0100

    connecting to database

--- util/database.js
+++ util/database.js
@@ -5,7 +5,7 @@ let _db;
 
 const mongoConnect = (callback) => {
   MongoClient.connect(
-    "mongodb+srv://node-user:db-pass@cluster-0.1b4j4.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster-0"
+    'mongodb+srv://node-user:db-pass@cluster-0.1b4j4.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster-0'
   )
     .then((client) => {
       console.log("Connected!");
