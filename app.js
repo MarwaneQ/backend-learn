68b26313afe5828d79572d5fc528af5887249cc2
MarwaneQ <merouane12345bensadallah@gmail.com>
2024-09-07 15:53:53 +0100

    encryptin password

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
-
+const bcrypt = require("bcryptjs");
 const errorController = require("./controllers/error");
 const User = require("./models/user");
 
